b20dc630a5a91f23cb5aad2491da819bf7a05cf6
Sorin Muntean <me@sorinmuntean.ro>
2020-02-04 21:21:23 +0100

    basic blog #2

--- main.rkt
+++ main.rkt
@@ -1,12 +1,5 @@
 #lang web-server/insta
 
-(define (start request)
-  (response/xexpr
-   '(html
-     (head (title "HELLO"))
-     (body (h1 "Under construction")))))
-
-
 (struct post (title body))
 
 (define BLOG (list (post "First Post!" "This is my first post, la dee da")
@@ -15,9 +8,19 @@
 (define (start request)
   (render-blog-page BLOG request))
 
+
+(define (render-blog-page a-blog request)
+  (response/xexpr
+   `(html (head (title "My Blog"))
+          (body (h1 "My Blog")
+                ,(render-posts a-blog)))))
+ 
 (define (render-post a-post)
   `(div ((class "post"))
     (h1 ,(post-title a-post))
     (p ,(post-body a-post))))
 
  
+(define (render-posts a-blog)
+  `(div ((class "posts"))
+        ,@(map render-post a-blog)))
