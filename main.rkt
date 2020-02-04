ebca49dfc26f40fc01dd607efec4441046ba0dba
Sorin Muntean <me@sorinmuntean.ro>
2020-02-04 21:23:29 +0100

    blog with form

--- main.rkt
+++ main.rkt
@@ -6,21 +6,43 @@
                    (post "Second post!" "This is my second post?!")))
 
 (define (start request)
-  (render-blog-page BLOG request))
+  (define a-blog
+    (cond [(can-parse-post? (request-bindings request))
+           (cons (parse-post (request-bindings request))
+                 BLOG)]
+          [else
+           BLOG]))
+  (render-blog-page a-blog request))
 
 
 (define (render-blog-page a-blog request)
+  (display "request start")
+  (display request)
+  (display "request end")
   (response/xexpr
    `(html (head (title "My Blog"))
           (body (h1 "My Blog")
-                ,(render-posts a-blog)))))
+                ,(render-posts a-blog)
+                (form
+                 (input ((name "title")))
+                 (input ((name "body")))
+                 (input ((type "submit"))))))))
  
 (define (render-post a-post)
   `(div ((class "post"))
-    (h1 ,(post-title a-post))
-    (p ,(post-body a-post))))
+        (h1 ,(post-title a-post))
+        (p ,(post-body a-post))))
 
  
 (define (render-posts a-blog)
   `(div ((class "posts"))
         ,@(map render-post a-blog)))
+
+
+(define (can-parse-post? bindings)
+  (and (exists-binding? 'title bindings)
+       (exists-binding? 'body bindings)))
+
+(define (parse-post bindings)
+  (post (extract-binding/single 'title bindings)
+        (extract-binding/single 'body bindings)))
